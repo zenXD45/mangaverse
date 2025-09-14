2a1181bcc7fb31505e910c28b7860911799272f8
Parag Manwal <mparag838@gmail.com>
2025-09-14 13:12:45 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's2.mangadex.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
